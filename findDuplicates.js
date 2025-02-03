cfac947ff1f3e410593ce0e2b8892870943e1d4f
unknown <arshia.org79@gmail.com>
2025-02-03 14:33:05 +0330

    for second branch ex:find with reduce

--- findDuplicates.js
+++ findDuplicates.js
@@ -12,4 +12,14 @@
 
 // Write your solution here
 
+function findDuplicates(arr) {
+    const count = arr.reduce((acc, num) => {
+        acc[num] = (acc[num] || 0) + 1;
+        return acc;
+    }, {});
+    return Object.keys(count).filter(num => count[num] > 1).map(Number);
+}
+
+console.log(findDuplicates([1, 2, 2, 3, 4, 4])); // [2, 4]
 module.exports = findDuplicates;
+
