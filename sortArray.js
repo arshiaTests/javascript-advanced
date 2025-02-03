8cf511e386fdb7ad67d0a0657bf658fdad730e82
unknown <arshia.org79@gmail.com>
2025-02-03 17:05:52 +0330

    second branch for sort array

--- sortArray.js
+++ sortArray.js
@@ -8,5 +8,24 @@
  * Example:
  * sortArray([3, 1, 4, 1, 5, 9]) should return [1, 1, 3, 4, 5, 9].
  */
-
-module.exports = sortArray;
+function sortArray(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      let minIndex = i;
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[minIndex]) {
+          minIndex = j;
+        }
+      }
+      if (minIndex !== i) {
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]; // Swap elements
+      }
+    }
+    return arr;
+  }
+  console.log(sortArray([3, 1, 4, 1, 5, 9])); // [1, 1, 3, 4, 5, 9]
+  console.log(sortArray([10, 2, 5, 1])); // [1, 2, 5, 10]
+  
+  module.exports = sortArray;
+  
+ 
+  
